5f2c77bee5b4132c46b38c218bb9277dc778bf44
Steven Han <nazgul33@gmail.com>
2016-04-21 17:46:28 +0900

    support impala 2.5.0

--- impala-package.sh
+++ impala-package.sh
@@ -41,11 +41,13 @@ echo "Impala install path : $instpath"
 echo "Impala build type : $buildtype"
 echo "Impala source path : $srcpath"
 
+impala_gcclib_path=$srcpath/toolchain/gcc-4.9.2/lib64
 impala_home=$instpath
 impala_shell=$instpath/impala-shell
 mkdir -p $impala_home/bin
 mkdir -p $impala_home/cloudera
 mkdir -p $impala_home/lib
+mkdir -p $impala_home/lib64
 mkdir -p $impala_home/log
 mkdir -p $impala_home/llvm-ir
 mkdir -p $impala_home/sbin-debug
@@ -62,10 +64,14 @@ else
 fi
 
 # HOME/sbin
+# since impala 2.5.0,
+# all binaries are fused into impalad, catalogd and statestred are symlnks
 $CP $be_src_path/service/impalad $be_dst_path
 $CP $be_src_path/service/libfesupport.so $be_dst_path
-$CP $be_src_path/catalog/catalogd $be_dst_path
-$CP $be_src_path/statestore/statestored $be_dst_path
+#$CP $be_src_path/catalog/catalogd $be_dst_path
+#$CP $be_src_path/statestore/statestored $be_dst_path
+ln -s impalad $be_dst_path/catalogd
+ln -s impalad $be_dst_path/statestored
 
 if [[ "$buildtype" == "release" ]]; then
 	$CP $be_src_path/service/impalad $be_dst_path
@@ -79,10 +85,11 @@ $CP -r $srcpath/llvm-ir/* $impala_home/llvm-ir/
 
 # HOME/lib
 $CP $srcpath/fe/target/dependency/* $impala_home/lib/
-$CP $srcpath/thirdparty/hadoop-*-cdh*/lib/native/libhadoop*.so* $impala_home/lib/
-$CP $srcpath/thirdparty/hadoop-*-cdh*/lib/native/libhdfs*.so* $impala_home/lib/
 $CP $srcpath/thirdparty/hadoop-*-cdh*/share/hadoop/common/lib/junit-*.jar $impala_home/lib/
 $CP $srcpath/fe/target/impala-frontend-0.1-SNAPSHOT.jar $impala_home/lib/
+# HOME/lib64
+$CP $srcpath/thirdparty/hadoop-*-cdh*/lib/native/libhadoop*.so* $impala_home/lib64/
+$CP $srcpath/thirdparty/hadoop-*-cdh*/lib/native/libhdfs*.so* $impala_home/lib64/
 
 # HOME/cloudera
 if [ -e $srcpath/cloudera/cdh_version.properties ]; then
@@ -99,17 +106,31 @@ $CP -r $srcpath/shell/build/impala-shell-*-cdh*/gen-py $impala_shell/
 $CP -r $srcpath/shell/build/impala-shell-*-cdh*/lib $impala_shell/
 
 # copy other libs needed
+export LD_LIBRARY_PATH=$impala_gcclib_path:$LD_LIBRARY_PATH
 ldd ${be_dst_path}/impalad | grep libboost | awk '{ print $3 }' > .tmp.boost.libs.list
-ldd ${be_dst_path}/catalogd | grep libboost | awk '{ print $3 }' >> .tmp.boost.libs.list
-ldd ${be_dst_path}/statestored | grep libboost | awk '{ print $3 }' >> .tmp.boost.libs.list
 cat .tmp.boost.libs.list | sort -u > .tmp.boost.libs.sorted.unique
 for f in `cat .tmp.boost.libs.sorted.unique`; do
-	$CP $f $impala_home/lib/
+	# might be a link. copy full file
+	cp $f $impala_home/lib64/
 	linksource=`basename $f`
-	linktarget=$impala_home/lib/`basename $f | sed 's/\.so\.[0-9]*\.[0-9]*\.[0-9]*/.so/'`
+	linktarget=$impala_home/lib64/`basename $f | sed 's/\.so\.[0-9]*\.[0-9]*\.[0-9]*/.so/'`
 	ln -sfv $linksource $linktarget
 done
 rm -f .tmp.boost.libs.list .tmp.boost.libs.sorted.unique
+
+# copy libsasl2
+ldd ${be_dst_path}/impalad | grep libsasl2 | awk '{ print $3 }' > .tmp.libsasl2.list
+for f in `cat .tmp.libsasl2.list`; do
+	# might be a link. copy full file
+	cp $f $impala_home/lib64/
+	linksource=`basename $f`
+	linktarget=$impala_home/lib64/`basename $f | sed 's/\.so\.[0-9]*/.so/'`
+	ln -sfv $linksource $linktarget
+done
+rm -f .tmp.libsasl2.list
+
+# libc++/etc
+$CP $impala_gcclib_path/*.so* $impala_home/lib64/
 
 # overwrite files in src/
 $CP -r src/* $impala_home/
